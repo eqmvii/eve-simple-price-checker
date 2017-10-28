815719ea7160856d0de016d2b2b7e5d97c96eb24
eqmvii <coelomate@gmail.com>
2017-10-28 13:02:31 -0400

    buy/sell prices added

--- server.js
+++ server.js
@@ -14,65 +14,288 @@ app.get('/getmineralprices', function (req, httpRes) {
     console.log("Mineral prices endpoint hit");
     var prices = {};
 
-     // https://esi.tech.ccp.is/latest/markets/prices/?datasource=tranquility
-     fetch("https://esi.tech.ccp.is/latest/markets/prices/?datasource=tranquility")
-     .then(res => {
-         if (res.ok){
-             console.log("Got a response!");
-             return res.json();
-         }
-         else { throw Error (res.statusText)}
-     })
-     .then(res => {
-         console.log(res.length);
-         for (let i = 0; i < res.length; i++){
-             if (res[i].type_id == 34){
-                 console.log("Trit: " + res[i].average_price);
-                 prices.tritanium = res[i].average_price;
-             } 
-             else if (res[i].type_id == 35) {
-                console.log("Pyer: " + res[i].average_price);
-                prices.pyerite = res[i].average_price;
-             }
-             else if (res[i].type_id == 36) {
-                console.log("Mex: " + res[i].average_price);
-                prices.mexallon = res[i].average_price;
-             }
-             else if (res[i].type_id == 37) {
-                console.log("Iso: " + res[i].average_price);
-                prices.isogen = res[i].average_price;
-             }
-             else if (res[i].type_id == 38) {
-                console.log("Mex: " + res[i].average_price);
-                prices.nocxium = res[i].average_price;
-             }
-             else if (res[i].type_id == 39) {
-                console.log("Mex: " + res[i].average_price);
-                prices.zydrine = res[i].average_price;
-             }
-             else if (res[i].type_id == 40) {
-                console.log("Mex: " + res[i].average_price);
-                prices.megacyte = res[i].average_price;
-             }
-             else if (res[i].type_id == 11399) {
-                console.log("Morph: " + res[i].average_price);
-                prices.morphite = res[i].average_price;
-             }             
-         }
-         httpRes.json(prices);
-     })
-     .catch(err => console.log(err));
+    // https://esi.tech.ccp.is/latest/markets/prices/?datasource=tranquility
+    fetch("https://esi.tech.ccp.is/latest/markets/prices/?datasource=tranquility")
+        .then(res => {
+            if (res.ok) {
+                console.log("Got a response!");
+                return res.json();
+            }
+            else { throw Error(res.statusText) }
+        })
+        .then(res => {
+            console.log(res.length);
+            for (let i = 0; i < res.length; i++) {
+                if (res[i].type_id == 34) {
+                    console.log("Trit: " + res[i].average_price);
+                    prices.tritanium = res[i].average_price;
+                }
+                else if (res[i].type_id == 35) {
+                    console.log("Pyer: " + res[i].average_price);
+                    prices.pyerite = res[i].average_price;
+                }
+                else if (res[i].type_id == 36) {
+                    console.log("Mex: " + res[i].average_price);
+                    prices.mexallon = res[i].average_price;
+                }
+                else if (res[i].type_id == 37) {
+                    console.log("Iso: " + res[i].average_price);
+                    prices.isogen = res[i].average_price;
+                }
+                else if (res[i].type_id == 38) {
+                    console.log("Nocx: " + res[i].average_price);
+                    prices.nocxium = res[i].average_price;
+                }
+                else if (res[i].type_id == 39) {
+                    console.log("Zyd: " + res[i].average_price);
+                    prices.zydrine = res[i].average_price;
+                }
+                else if (res[i].type_id == 40) {
+                    console.log("Meg: " + res[i].average_price);
+                    prices.megacyte = res[i].average_price;
+                }
+                else if (res[i].type_id == 11399) {
+                    console.log("Morph: " + res[i].average_price);
+                    prices.morphite = res[i].average_price;
+                }
+            }
+            httpRes.json(prices);
+        })
+        .catch(err => console.log(err));
 
-    
-  });
+});
+
+var buy_prices = [];
+var sell_prices = [];
+
+var jita_prices = {
+    tritanium: {
+        name: "tritanium",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    pyerite: {
+        name: "pyerite",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    mexallon: {
+        name: "mexallon",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    isogen: {
+        name: "isogen",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    nocxium: {
+        name: "nocxium",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    zydrine: {
+        name: "zydrine",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    megacyte: {
+        name: "megacyte",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    },
+    morphite: {
+        name: "morphite",
+        lowest_sell: Infinity,
+        highest_buy: 0
+    }
+};
+
+function process_orders(data, typeid) {
+    //console.log(data.length);
+
+    // array of the typids we are interested in
+    var typeids = [34, 35, 36, 37, 38, 39, 40, 11399];
+
+    for (let i = 0; i < data.length; i++) {
+        // Only look for orders of our item type in our station (Jita 4-4)
+        if (data[i].location_id == 60003760) {
+            // Only do something if this is one of our type_ids
+            if (typeids.indexOf(data[i].type_id) >= 0) {
+                // console.log("Found a matching order!");
+                // console.log(data[i].type_id);
+                var order_type;
+                if (data[i].is_buy_order) {
+                    order_type = "buy";
+                    // buy_prices.push(data[i].price);
+                } else {
+                    order_type = "sell";
+                    //sell_prices.push(data[i].price);
+                }
+                switch (data[i].type_id) {
+                    case 34:
+                        // console.log("Found a trit order!");
+                        if (order_type == "buy" && data[i].price > jita_prices.tritanium.highest_buy) {
+                            jita_prices.tritanium.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.tritanium.lowest_sell) {
+                            jita_prices.tritanium.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 35:
+                        if (order_type == "buy" && data[i].price > jita_prices.pyerite.highest_buy) {
+                            jita_prices.pyerite.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.pyerite.lowest_sell) {
+                            jita_prices.pyerite.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 36:
+                        if (order_type == "buy" && data[i].price > jita_prices.mexallon.highest_buy) {
+                            jita_prices.mexallon.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.mexallon.lowest_sell) {
+                            jita_prices.mexallon.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 37:
+                        if (order_type == "buy" && data[i].price > jita_prices.isogen.highest_buy) {
+                            jita_prices.isogen.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.isogen.lowest_sell) {
+                            jita_prices.isogen.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 38:
+                        if (order_type == "buy" && data[i].price > jita_prices.nocxium.highest_buy) {
+                            jita_prices.nocxium.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.nocxium.lowest_sell) {
+                            jita_prices.nocxium.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 39:
+                        if (order_type == "buy" && data[i].price > jita_prices.zydrine.highest_buy) {
+                            jita_prices.zydrine.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.zydrine.lowest_sell) {
+                            jita_prices.zydrine.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 40:
+                        if (order_type == "buy" && data[i].price > jita_prices.megacyte.highest_buy) {
+                            jita_prices.megacyte.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.megacyte.lowest_sell) {
+                            jita_prices.megacyte.lowest_sell = data[i].price;
+                        }
+                        break;
+                    case 11399:
+                        if (order_type == "buy" && data[i].price > jita_prices.morphite.highest_buy) {
+                            jita_prices.morphite.highest_buy = data[i].price;
+                        }
+                        else if (order_type == "sell" && data[i].price < jita_prices.morphite.lowest_sell) {
+                            jita_prices.morphite.lowest_sell = data[i].price;
+                        }
+                        break;
+                }
+                // console.log("Order: " + order_type + " " + data[i].volume_remain + " at " + data[i].price);  
+            }
+        }
+    }
+}
+
+//getjitamineralsell
+// Get jita mineral sell prices from the Eve: Online ESI API
+app.get('/getjitamineralsell', function (req, httpRes) {
+    console.log("Mineral SELL prices endpoint hit");
+    var prices = {};
+    var orders_fetched = 0;
+
+    // the forge region id: 10000002
+    // jita structure id: 60003760
+
+    // region orders URL
+    var orders_url = "https://esi.tech.ccp.is/latest/markets/10000002/orders/?datasource=tranquility&order_type=all&page="
+    var pages_fetched = 0;
+    var page_to_fetch = 1;
+    var max_pages = false;
+
+    // fetch the first page to find how many pages total to fetch
+    fetch(orders_url + page_to_fetch)
+        .then(res => {
+            if (res.ok) {
+                // console.log("Got a response for page " + page_to_fetch);
+                if (max_pages == false) {
+                    // console.log("Headers: ");
+                    // console.log(res.headers.get("x-pages"));
+                    max_pages = parseInt(res.headers.get("x-pages"), 10);
+                }
+                return res.json();
+            } else { throw Error(res.statusText) }
+        })
+        .then(res => {
+            // console.log("Order response information:");
+            // console.log("Page length: " + res.length);
+            orders_fetched += res.length;
+            // console.log("Headers:");
+            // console.log(res.headers);
+            process_orders(res, 38);
+            pages_fetched += 1;
+            page_to_fetch += 1;
+            // console.log("Fetched: " + pages_fetched + "/" + max_pages);
+            return res;
+        })
+        .then(() => {
+            for (let i = page_to_fetch; i <= max_pages; i++) {
+                // fetchy fetchy 
+                fetch(orders_url + i)
+                    .then(res => {
+                        if (res.ok) {
+                            // console.log("Got a response!");
+                            // pages_fetched += 1;
+                            // console.log(pages_fetched);
+                            // console.log(typeof res.json())
+                            return res.json();
+                        } else { throw Error(res.statusText) }
+                    })
+                    .then(res => {
+                        pages_fetched += 1;
+                        orders_fetched += res.length;
+                        console.log(orders_fetched + " (" + pages_fetched + "/" + max_pages + ")");
+                        process_orders(res, 38);
+                        if (pages_fetched == max_pages) {
+                            // buy_prices.sort(function (a, b) { return a - b });
+                            // sell_prices.sort(function (a, b) { return a - b });
+                            // console.log("Highest buy: " + buy_prices[buy_prices.length - 1]);
+                            // console.log("Lowest sell: " + sell_prices[0]);
+                            console.log("Jita prices: ");
+                            for (var item in jita_prices) {
+                                console.log(item + ": " + jita_prices[item].highest_buy + " <-> " + jita_prices[item].lowest_sell);
+                            }
+                            httpRes.json(jita_prices);
+                        }
+
+                        // console.log("Page " + pages_fetched + ". Orders fetched: " + orders_fetched);                
+                    })
+                    .catch(err => { console.log(err) });
+
+            }
+
+        })
+        .catch(err => { console.log(err) });
+
+
+
+});
 
 // API endpoint for testing
 app.get('/test', function (req, res) {
     console.log("test endpoint hit");
-   
+
 
     res.json("Success! This text delivered from the server.");
-  });
+});
 
 // Start up the server:
 app.listen(port, function () {
--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.fetchMineralPrices = this.fetchMineralPrices.bind(this);
 
     this.state = {
-      serverStatus: "No request/response from the server yet...",
+      serverStatus: "No response from the server yet...",
       tritanium: 0,
       pyerite: 0,
       isogen: 0,
@@ -22,7 +22,24 @@ class App extends Component {
       nocxium: 0,
       zydrine: 0,
       megacyte: 0,
-      morphite: 0
+      morphite: 0,
+      tritaniumsell: 0,
+      pyeritesell: 0,
+      isogensell: 0,
+      mexallonsell: 0,
+      nocxiumsell: 0,
+      zydrinesell: 0,
+      megacytesell: 0,
+      morphitesell: 0,
+      tritaniumbuy: 0,
+      pyeritebuy: 0,
+      isogenbuy: 0,
+      mexallonbuy: 0,
+      nocxiumbuy: 0,
+      zydrinebuy: 0,
+      megacytebuy: 0,
+      morphitebuy: 0,
+      disableRefresh: false,
     };
 
     // bind this for use in below callback
@@ -39,17 +56,20 @@ class App extends Component {
         }
       })
       .then(function (res) {
-        console.log(res);
-        that.setState({ serverStatus: "Server connected!" });
-      }).catch(err => console.log(err));
-
-    this.fetchMineralPrices();
+        //console.log(res);
+        that.setState({ serverStatus: "Server connected, fetching data" });
+      }).catch(err => console.log(err));   
 
   }
 
-  fetchMineralPrices() {
+  componentWillMount() {
+    this.fetchMineralPrices();
+  }
 
-    // get price data
+  fetchMineralPrices() {
+    this.setState({serverStatus: "Fetching price data...", disableRefresh: true});
+
+    // get universe price data
     fetch('/getmineralprices')
       .then(res => {
         if (res.ok) {
@@ -59,7 +79,7 @@ class App extends Component {
         }
       })
       .then(res => {
-        console.log(res);
+        // console.log(res);
         this.setState({
           tritanium: USD.format(parseFloat(res.tritanium, 10)),
           pyerite: USD.format(parseFloat(res.pyerite, 10)),
@@ -69,23 +89,73 @@ class App extends Component {
           nocxium: USD.format(parseFloat(res.nocxium, 10)),
           zydrine: USD.format(parseFloat(res.zydrine, 10)),
           morphite: USD.format(parseFloat(res.morphite, 10)),
+          serverStatus: "Universe prices fetched... fetching jita buy/sell prices"
         });
-      }).catch(err => console.log(err));
-
+        // get Jita buy and sell data
+        fetch('/getjitamineralsell')
+          .then(res => {
+            if (res.ok) {
+              return res.json();
+            } else {
+              throw Error(res.statusText)
+            }
+          })
+          .then(res => {
+            // console.log(res);
+            this.setState({
+              tritaniumsell: USD.format(parseFloat(res.tritanium.lowest_sell, 10)),
+              pyeritesell: USD.format(parseFloat(res.pyerite.lowest_sell, 10)),
+              isogensell: USD.format(parseFloat(res.isogen.lowest_sell, 10)),
+              mexallonsell: USD.format(parseFloat(res.mexallon.lowest_sell, 10)),
+              megacytesell: USD.format(parseFloat(res.megacyte.lowest_sell, 10)),
+              nocxiumsell: USD.format(parseFloat(res.nocxium.lowest_sell, 10)),
+              zydrinesell: USD.format(parseFloat(res.zydrine.lowest_sell, 10)),
+              morphitesell: USD.format(parseFloat(res.morphite.lowest_sell, 10)),
+              tritaniumbuy: USD.format(parseFloat(res.tritanium.highest_buy, 10)),
+              pyeritebuy: USD.format(parseFloat(res.pyerite.highest_buy, 10)),
+              isogenbuy: USD.format(parseFloat(res.isogen.highest_buy, 10)),
+              mexallonbuy: USD.format(parseFloat(res.mexallon.highest_buy, 10)),
+              megacytebuy: USD.format(parseFloat(res.megacyte.highest_buy, 10)),
+              nocxiumbuy: USD.format(parseFloat(res.nocxium.highest_buy, 10)),
+              zydrinebuy: USD.format(parseFloat(res.zydrine.highest_buy, 10)),
+              morphitebuy: USD.format(parseFloat(res.morphite.highest_buy, 10)),
+              serverStatus: 'Buy/sell prices fetched, table updated!',
+              disableRefresh: false
+            });
+          }).catch(err => {
+            console.log(err);
+            this.setState({serverStatus: "Error pulling prices. Trying again..."});
+            setTimeout(this.fetchMineralPrices, 2000);
+          });
+      }).catch(err => {
+        console.log(err);
+        this.setState({serverStatus: "Error pulling prices. Trying again..."});
+        setTimeout(this.fetchMineralPrices, 2000);
+      });
   }
 
   render() {
+    var status_icon;
+    var refresh_button;
+    if (this.state.disableRefresh){
+      status_icon = (<i className="fa fa-spinner fa-spin" style={{fontSize: "20px"}}></i>);
+      refresh_button = (<button className="btn btn-primary btn-lg disabled">Refresh Prices</button>);
+    } else {
+      refresh_button = (<button className ="btn btn-primary btn-lg" onClick={this.fetchMineralPrices}>Refresh Prices</button>)
+      status_icon = (<span className="glyphicon glyphicon-ok" style={{color:"green", fontSize: "20px"}}aria-hidden="true"></span>);
+    }
     return (
       <div className="container">
         <div className="row">
           <div className="jumbotron text-center">
-            <h1>Eve: Online Price Check</h1>
-            <h4>Market prices at a glance from the Eve: Online API</h4>
+            <h1>Eve: Online Mineral Prices</h1>
+            <h4>Market prices at a glance from the Eve ESI API</h4>            
           </div>
         </div>
         <div className="row">
-          <p className="text-center">Hello, Eve-world! React is rendering this JSX code.</p>
-          <p className="text-center">Server response: {this.state.serverStatus} </p>
+          <h3 className="text-center"><strong>Data Status</strong></h3>
+          <p className="text-center">{status_icon}</p>
+          <p className="text-center">{this.state.serverStatus} </p>
           <br />
           <table className="table table-condensed">
             <tbody>
@@ -101,7 +171,29 @@ class App extends Component {
                 <td className="col-md-1"><strong>Morphite</strong></td>
               </tr>
               <tr>
-                <td className="col-md-1"><strong>Current Price</strong></td>
+                <td className="col-md-1"><strong>Jita Buy</strong></td>
+                <td className="col-md-1">{this.state.tritaniumbuy}</td>
+                <td className="col-md-1">{this.state.pyeritebuy}</td>
+                <td className="col-md-1">{this.state.isogenbuy}</td>
+                <td className="col-md-1">{this.state.mexallonbuy}</td>
+                <td className="col-md-1">{this.state.nocxiumbuy}</td>
+                <td className="col-md-1">{this.state.zydrinebuy}</td>
+                <td className="col-md-1">{this.state.megacytebuy}</td>
+                <td className="col-md-1">{this.state.morphitebuy}</td>
+              </tr>
+              <tr>
+                <td className="col-md-1"><strong>Jita Sell</strong></td>
+                <td className="col-md-1">{this.state.tritaniumsell}</td>
+                <td className="col-md-1">{this.state.pyeritesell}</td>
+                <td className="col-md-1">{this.state.isogensell}</td>
+                <td className="col-md-1">{this.state.mexallonsell}</td>
+                <td className="col-md-1">{this.state.nocxiumsell}</td>
+                <td className="col-md-1">{this.state.zydrinesell}</td>
+                <td className="col-md-1">{this.state.megacytesell}</td>
+                <td className="col-md-1">{this.state.morphitesell}</td>
+              </tr>
+              <tr>
+                <td className="col-md-1"><strong>Universe Price</strong></td>
                 <td className="col-md-1">{this.state.tritanium}</td>
                 <td className="col-md-1">{this.state.pyerite}</td>
                 <td className="col-md-1">{this.state.isogen}</td>
@@ -111,8 +203,18 @@ class App extends Component {
                 <td className="col-md-1">{this.state.megacyte}</td>
                 <td className="col-md-1">{this.state.morphite}</td>
               </tr>
+
             </tbody>
           </table>
+          <br />
+          <div className="text-center">
+          {refresh_button}
+          </div>
+          <br />
+          <br />
+          <p className="col-sm-6 col-md-offset-3">Data pulled from the <a href="https://esi.tech.ccp.is/latest/">ESI API</a>. <strong>Universe Price</strong> is the price returned by the generic price API. <strong>Jita Buy</strong> is the highest buy order located in Jita 4-4, regardless of volume. <strong>Jita Sell</strong> is the lowest sell order located in Jita 4-4, regardless of volume. Prices will not reflect regional buy/sell orders, even if they can be filled in Jita 4-4.</p>
+          <br />
+
         </div>
       </div>
     );
